171a6fb73a8d4ae8274a0cf68604f86364af746e
Narendra Joshi <narendraj9@gmail.com>
2015-02-07 17:18:39 +0530

    Day1

---
+++ code/day1/GettingStarted.hs
@@ -0,0 +1,17 @@
+
+
+alist = [1,2,3,4,5]
+takeThree = take 3
+
+anotherList = [10..20]
+takeFive = take 5
+
+
+second xs = head (tail xs)
+third xs = head (tail (tail xs))
+
+cube xeta = xeta * xeta * xeta
+
+swap (x, y) = (y, x)
+
+pair x y = (x, y)
